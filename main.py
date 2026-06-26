a535a8a518dba1536c6676943752900b90b68a65
Stuart Williams <spookdejur@hotmail.com>
2026-06-26 13:37:33 -0700

    works,1

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import json
+import sys
+import os
+import pandas as pd
+
+
 def main():
-    print("Hello from json2keylist!")
+    """
+    Read a file as JSON, into a DICT, sort by key, emit STDOUT
+    """
+    # Ensure the user provided the file argument
+    if len(sys.argv) < 2:
+        print("Error: Please provide a file path.")
+        sys.exit(1)
+
+    # Retrieve the file path from the first position
+    file_path = sys.argv[1]
+
+    if not os.path.isfile(file_path):
+        print("Not a valid file: ", file_path)
+        sys.exit(2)
+
+    # Open the file and parse it directly into a dictionary
+    with open(file_path, "r") as file:
+        file_content = file.read()
+
+    data_dict = json.loads(file_content)
+    df = pd.json_normalize(data_dict)
+    flat_dict = df.to_dict(orient="records")[0]
+
+    print(flat_dict)
 
 
 if __name__ == "__main__":
